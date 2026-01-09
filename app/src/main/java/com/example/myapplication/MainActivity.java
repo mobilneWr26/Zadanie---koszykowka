f0bc0cde13cf983598a1597d8d43d5ebf7717da8
mobilneWr26 <wiktor.rojek@uczen.mechaniktg.pl>
2026-01-09 11:12:27 +0100

    Liczenie punktów

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +13,48 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button1, button2, button3;
+    private TextView textViewPunkty;
+    private int punkty;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        textViewPunkty = findViewById(R.id.textViewPunkty);
+        button1 = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
 
+        textViewPunkty.setText(String.valueOf(punkty));
+        punkty = 0;
+        button1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty++;
+                        textViewPunkty.setText(String.valueOf(punkty));
+                    }
+                }
+        );
+        button2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty += 2;
+                        textViewPunkty.setText(String.valueOf(punkty));
+                    }
+                }
+        );
+        button3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty += 3;
+                        textViewPunkty.setText(String.valueOf(punkty));
+                    }
+                }
+        );
     }
 }
